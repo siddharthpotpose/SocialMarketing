18bf399d68444ed4f801d62f11a258ac875843c5
siddharthpotpose <siddharthpotpose9@gmail.com>
2023-05-06 16:30:46 +0530

    new

---
+++ js/main.js
@@ -0,0 +1,36 @@
+ 
+
+ let nav = document.querySelector("navbar")
+ window.onscroll=function (){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled")
+    }
+    else{
+        nav.classList.remove("header-scrolled")
+
+    }
+ }
+
+//  owl 
+$(document).ready(function(){
+    $('.client-slider-section').owlCarousel({
+        items:4,
+        loop:true,
+        nav:false,
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:6
+            }
+        }
+    })
+})
